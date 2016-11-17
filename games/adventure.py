bbfbafae766267c7cda37ba458f9fe3484231be0
Derek Moyes <derek.moyes@rackspace.com>
2016-11-16 21:03:45 -0600

    add tombstone

--- games/adventure.py
+++ games/adventure.py
@@ -2,17 +2,10 @@ import random
 
 basementClosetDoor = "closed"
 currentLocation = "basement"
-debug = True
+debug = False
 done = False
 equipped = ""
-nouns = ["cue", "door", "exit", "kitchen", "stairs", "window"]
-verbs = ["drop", "climb", "close", "go", "grab", "look", "open", "put", "take"]
-
-playerName = input("Who goes there? (Enter your name) ")
-print("Welcome to Adventure, " + playerName + "!\n")
-
 gameHelp = "Use <verb> <noun> to do stuff. You can also look to repeat the description of your location."
-
 greeting = """You awake in what appears to be the basement of your local pub.
 
 Your head hurts and you seem to be alone.
@@ -20,15 +13,40 @@ Your head hurts and you seem to be alone.
 As the windows are painted over, you can't tell exactly what time it is.
 
 Oh, and your watch seems to be missing.\n"""
+nouns = ["cue", "door", "exit", "kitchen", "stairs", "window"]
+verbs = ["drop", "climb", "close", "go", "grab", "look", "open", "put", "take"]
+# new verbs: run, get, enter
+
+def clearScreen():
+    print ("\n" * 100)
+    return
 
 def deathText():
     "deathText randomly picks a message to display when the player dies, and the game is over."
+    action = ""
     deaths = ["Ow, that left a mark.",
               "Well, that certainly wasn't a good idea!",
               "That really hurt.",
               "Ow."]
-    print(random.choice(deaths))
-    print("\nYou are dead.")
+    deathName = playerName.center(11, " ")
+    tombstone = """
+        _.---,._,'
+       /' _.--.<
+         /'     `'
+       /' _.---._____
+       \.'   ___, .-'`
+           /'    \\              .
+         /'       `-.          -|-
+        |                       |
+        |                   .-'~~~`-.
+        |                 .'         `.
+        |                 |  R  I  P  |
+        |                 |           |
+        |                 |""" + deathName + "|" + """
+         \              \\\\|           |//               jgs
+   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+    print(tombstone + random.choice(deaths) + "\nYou are dead.")
     return
 
 def failText():
@@ -36,20 +54,33 @@ def failText():
     fails = ["That wouldn't be prudent.",
              "Not a good idea!",
              "That would leave a mark.",
-             "Really? I dont't think so."]
+             "Really?"]
     print(random.choice(fails))
     return
+
+clearScreen()
+playerName = raw_input("Who goes there? (Enter your name) ")
+print("Welcome to Adventure, " + playerName + "!\n")
 
 print(greeting)
 while not done:
     if currentLocation == "basement":
-        locationtext = "You seem to be in the basement. You see stairs going up, and a door on the left."
+        locationtext = "You seem to be in the basement. You see stairs going " \
+                       "up, and a door on the left."
         if equipped == "":
             locationtext = locationtext + " There is a broken pool cue here."
     if currentLocation == "basementCloset":
-        locationtext = "You are now in the basement closet. There's a funny smell here. It's dark."
+        locationtext = "You are now in the basement closet. There's a funny " \
+                       "smell here. It's dark."
     if currentLocation == "pubHallway":
-        locationtext = "You are in a small hallway. There are stairs leading down into the basement, and what looks like an exit to the kitchen."
+        locationtext = "You are in a small hallway. There are stairs leading " \
+                       "down into the basement, and what looks like an exit " \
+                       "to the kitchen."
+    if currentLocation == "pubKitchen":
+        locationtext = "You are now in what looks like the pub's kitchen. " \
+                       "The pub must be closed, although the kitchen still " \
+                       "smells like supper. There's an exit to the hallway, " \
+                       "and a swinging door leads to the pub's Great Room."
     if currentLocation == "pubGreatRoom":
         locationtext = "The local"
 
@@ -58,7 +89,7 @@ while not done:
 
     action = ""
     thing = ""
-    command = input("What do you do?\n>>> ")
+    command = raw_input("What do you do?\n>>> ")
     if debug == True: print(command)
     if (command == "" or command == "help" or command == "Help"):
         print(gameHelp)
@@ -90,7 +121,8 @@ while not done:
             if thing == "door":
                 if basementClosetDoor == "open":
                     basementClosetDoor = "closed"
-                    print("The door to the closet creaks loudly as you close it behind you.")
+                    print("The door to the closet creaks loudly as you close "
+                          "it behind you.")
                     continue
                 else:
                     print("The door is already closed.")
@@ -128,7 +160,8 @@ while not done:
             if thing == "door":
                 if basementClosetDoor == "closed":
                     basementClosetDoor = "open"
-                    print("The door to the closet creaks loudly as you slowly open it. It's very dark in there.")
+                    print("The door to the closet creaks loudly as you "
+                          "slowly open it. It's very dark in there.")
                     continue
                 else:
                     print("It's already open, silly!")
@@ -142,10 +175,12 @@ while not done:
             if thing == "door":
                 if basementClosetDoor == "open":
                     basementClosetDoor = "closed"
-                    print("The door to the closet creaks loudly as you close it behind you. It's very dark in here.")
+                    print("The door to the closet creaks loudly as you "
+                          "close it behind you. It's very dark in here.")
                     continue
                 else:
-                    print("It's so dark in here the door must already be closed.")
+                    print("It's so dark in here the door must already be "
+                          "closed.")
                     continue
         if action == "go":
             if basementClosetDoor == "open":
@@ -153,8 +188,11 @@ while not done:
                 print("You walk into the basement.")
                 continue
             else:
-                print("The door is closed, but it doesn't seem locked.")
-                continue
+                clearScreen()
+                print("You fumble around in the dark and trip over the "
+                      "smelly, dirty mop bucket and fall down.")
+                deathText()
+                done = True
         if action == "open":
             if thing == "window":
                 print("You feel around a bit and don't locate any windows.")
@@ -162,7 +200,8 @@ while not done:
             if thing == "door":
                 if basementClosetDoor == "closed":
                     basementClosetDoor = "open"
-                    print("The door to the closet creaks loudly as you slowly open it. It's brighter out there.")
+                    print("The door to the closet creaks loudly as you slowly "
+                          "open it. It's brighter out there.")
                     continue
                 else:
                     print("It's already open, silly!")
@@ -180,13 +219,29 @@ while not done:
         if action == "go":
             if (thing == "exit" or thing == "kitchen"):
                 currentLocation = "pubKitchen"
-                print("You walk into the noisy kitchen and your eardrum bursts due to the awful noise.")
+                clearScreen()
+                print("You walk into the noisy kitchen and your eardrum "
+                      "bursts due to the awful noise.")
                 deathText()
                 done = True
                 continue
             if thing == "stairs":
                 currentLocation = "basement"
                 print("You climb the stairs.")
+                continue
+            else:
+                failText()
+                continue
+
+    if currentLocation == "pubKitchen":
+        if action == "go":
+            if (thing == "hallway"):
+                currentLocation = "pubHallway"
+                print("You walk back into the hallway.")
+                continue
+            if thing == "exit":
+                currentLocation = "pubGreatRoom"
+                print("You exit the kitchen into the Great Room.")
                 continue
             else:
                 failText()
