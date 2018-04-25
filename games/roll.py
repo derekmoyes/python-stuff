6d4b1da9efbaf77cddfdf6bb81318b63c3025e94
Derek Moyes <derek.moyes@rackspace.com>
2018-04-25 18:02:22 -0500

    Fixed.

--- games/roll.py
+++ games/roll.py
@@ -14,7 +14,7 @@ max = 6
 roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
-    print("Starting with", min, max, "size dice.")
+    print("Starting with", max, "sided dice.")
     roller = int(input("How many dice should I roll? "))
     print("Rolling the dice...")
     print("Let me roll those " + str(roller) + " dice for you!")
